3c8bb4d2f0e6bd6843ebcc1ac7823be01134c0e0
Trent Buckholz <trent.buckholz@c3.ai>
2025-10-05 10:13:39 -0700

    feat(zellij): Added zellij config

--- create_links.sh
+++ create_links.sh
@@ -4,3 +4,4 @@ ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/vim ~/.vimrc
 ln -s ~/dotfiles/.aerospace.toml ~/.aerospace.toml
+ln -s ~/dotfiles/zellij ~/.config/zellij
