694ede76c4da81fbcfeec7cb6bfa08ec30db5763
Trent Buckholz <trent.buckholz@c3.ai>
2025-10-13 20:22:35 -0700

    refactor: Changed existing functionality to use snacks.

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -3,8 +3,92 @@ return {
   ---@type snacks.Config
   opts = {
     lazygit = {},
+    notifier = {},
+    picker = {
+      ui_select = true,
+    },
+    terminal = {
+      win = {
+        style = 'float',
+      },
+    },
   },
   keys = {
+    {
+      '<leader>tt',
+      function()
+        Snacks.terminal()
+      end,
+      desc = 'Toggle Terminal',
+      mode = { 'n', 't' },
+    },
+
+    -- Picker.
+    {
+      '<leader>sR',
+      function()
+        Snacks.picker.resume()
+      end,
+      desc = 'Resume',
+    },
+    {
+      '<leader>sf',
+      function()
+        Snacks.picker.files()
+      end,
+      desc = 'Search Files',
+    },
+    {
+      '<leader>/',
+      function()
+        Snacks.picker.lines()
+      end,
+      desc = 'Buffer Lines',
+    },
+    {
+      '<leader>s/',
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = 'Grep',
+    },
+    {
+      '<leader>sw',
+      function()
+        Snacks.picker.grep_word()
+      end,
+      desc = 'Visual selection or word',
+      mode = { 'n', 'x' },
+    },
+    {
+      '<leader>sk',
+      function()
+        Snacks.picker.keymaps()
+      end,
+      desc = 'Keymaps',
+    },
+    {
+      '<leader>su',
+      function()
+        Snacks.picker.undo()
+      end,
+      desc = 'Undo History',
+    },
+    {
+      '<leader>sb',
+      function()
+        Snacks.picker.buffers()
+      end,
+      desc = 'Buffers',
+    },
+    {
+      '<leader>sd',
+      function()
+        Snacks.picker.diagnostics()
+      end,
+      desc = 'Diagnostics',
+    },
+    -- Other.
     {
       '<leader>bd',
       function()
