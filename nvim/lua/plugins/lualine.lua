220e2d834d9c387136c30fa3c7d05998c70a965d
Trent Buckholz <trent.buckholz@c3.ai>
2025-11-14 08:30:29 -0800

    feat(nvim): Add jj branch status in lualine.

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,7 +2,37 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
+    local function jj_branch()
+      if not vim.fn.executable 'jj' == 1 then
+        return ''
+      end
+
+      for i = 0, 5 do
+        local rev = '@'
+        rev = rev .. string.rep('-', i)
+        local cmd = string.format("jj log --no-pager -r '%s' --no-graph --template 'self.bookmarks()'", rev)
+        local handle = io.popen(cmd)
+        if not handle then
+          return ''
+        end
+        local result = handle:read '*a'
+        handle:close()
+        result = result:gsub('%s+', '')
+        if result ~= '' then
+          return 'jj:' .. result
+        end
+      end
+      return ''
+    end
     require('lualine').setup {
+      sections = {
+        lualine_b = {
+          jj_branch,
+          'branch',
+          'diff',
+          'diagnostics',
+        },
+      },
       options = {
         theme = 'auto',
         icons_enabled = vim.g.have_nerd_font,
