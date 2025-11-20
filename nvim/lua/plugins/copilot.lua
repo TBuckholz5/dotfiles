186a800a99596a54f17b234ffea5a3f093d1f621
Trent Buckholz <trent.buckholz@c3.ai>
2025-11-20 10:20:56 -0800

    feat(nvim): Added code companion as ai plugin.

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -3,19 +3,6 @@ return {
     'github/copilot.vim',
   },
   {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    dependencies = {
-      { 'nvim-lua/plenary.nvim', branch = 'master' },
-    },
-    build = 'make tiktoken',
-    opts = {
-      -- See Configuration section for options
-    },
-    keys = {
-      { '<leader>en', '<cmd>CopilotChatOpen<cr>', desc = 'Open Copilot Chat' },
-    },
-  },
-  {
     'ravitemer/mcphub.nvim',
     dependencies = {
       'nvim-lua/plenary.nvim',
@@ -24,5 +11,37 @@ return {
     config = function()
       require('mcphub').setup()
     end,
+  },
+  {
+    'olimorris/codecompanion.nvim',
+    dependencies = {
+      { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+      { 'nvim-lua/plenary.nvim' },
+      { 'ravitemer/mcphub.nvim' },
+    },
+    opts = {
+      extensions = {
+        mcphub = {
+          callback = 'mcphub.extensions.codecompanion',
+          opts = {
+            make_vars = true,
+            make_slash_commands = true,
+            show_result_in_chat = true,
+          },
+        },
+      },
+      strategies = {
+        chat = { adapter = 'copilot' },
+        inline = { adapter = 'copilot' },
+      },
+      opts = {
+        log_level = 'DEBUG',
+      },
+    },
+    keys = {
+      { '<leader>en', '<cmd>CodeCompanionChat Toggle<cr>', desc = 'Code Companion Chat' },
+      { '<leader>ei', ':CodeCompanion', desc = 'Code Companion Inline' },
+      { '<leader>ee', '<cmd>CodeCompanionActions<cr>', desc = 'Code Companion Actions' },
+    },
   },
 }
--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -3,5 +3,9 @@ return {
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
+  opts = {
+    preview = {
+      filetypes = { 'markdown', 'codecompanion' },
+    },
+  },
 }
