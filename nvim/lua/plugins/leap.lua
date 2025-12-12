95df3f0822ac7763a36d899d00163c8cb29b63b3
Trent Buckholz <trent.buckholz@c3.ai>
2025-12-12 15:12:15 -0800

    feat!(nvim): remove jj and leap plugins

--- nvim/lua/plugins/leap.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'ggandor/leap.nvim',
-  lazy = false,
-  config = function(_, opts)
-    require('leap').setup(opts)
-    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
-    vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
-  end,
-}
