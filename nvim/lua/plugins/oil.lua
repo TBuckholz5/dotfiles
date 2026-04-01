c9457a618c156ca36211d947a430bf442cc6b3a1
Trent Buckholz <trentjbuckholz@gmail.com>
2026-03-31 20:31:10 -0700

    feat(nvim): Change oil keybindings

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -6,7 +6,21 @@ return {
   config = function()
     require('oil').setup {
       keymaps = {
-        ['<Esc>'] = 'actions.parent',
+        ['<C-l>'] = 'actions.select',
+        ['<C-v>'] = { 'actions.select', opts = { vertical = true } },
+        ['<C-x>'] = { 'actions.select', opts = { horizontal = true } },
+        ['<C-t>'] = { 'actions.select', opts = { tab = true } },
+        ['<C-p>'] = 'actions.preview',
+        ['<C-c>'] = { 'actions.close', mode = 'n' },
+        ['<C-m>'] = 'actions.refresh',
+        ['<C-h>'] = { 'actions.parent', mode = 'n' },
+        ['_'] = { 'actions.open_cwd', mode = 'n' },
+        ['`'] = { 'actions.cd', mode = 'n' },
+        ['g~'] = { 'actions.cd', opts = { scope = 'tab' }, mode = 'n' },
+        ['gs'] = { 'actions.change_sort', mode = 'n' },
+        ['gx'] = 'actions.open_external',
+        ['g.'] = { 'actions.toggle_hidden', mode = 'n' },
+        ['g\\'] = { 'actions.toggle_trash', mode = 'n' },
       },
     }
   end,
