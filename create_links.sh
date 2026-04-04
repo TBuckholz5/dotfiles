37463622829b8a9d76281656385a5aaff2877c84
Trent Buckholz <trentjbuckholz@gmail.com>
2026-04-03 17:06:32 -0700

    feat(zellij): Add zellij config

--- create_links.sh
+++ create_links.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/nvim ~/.config/nvim
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.aerospace.toml ~/.aerospace.toml
-ln -s ~/dotfiles/.zshrc ~/.zshrc
+DOTFILES=~/dotfiles
+
+link() {
+    local src=$1
+    local dest=$2
+
+    if [ -L "$dest" ]; then
+        rm "$dest"
+    elif [ -e "$dest" ]; then
+        echo "WARNING: $dest exists and is not a symlink, skipping"
+        return
+    fi
+
+    ln -s "$src" "$dest"
+    echo "Linked $dest -> $src"
+}
+
+link "$DOTFILES/.tmux.conf"      ~/.tmux.conf
+link "$DOTFILES/nvim"            ~/.config/nvim
+link "$DOTFILES/.vimrc"          ~/.vimrc
+link "$DOTFILES/.aerospace.toml" ~/.aerospace.toml
+link "$DOTFILES/.zshrc"          ~/.zshrc
+link "$DOTFILES/zellij"          ~/.config/zellij
