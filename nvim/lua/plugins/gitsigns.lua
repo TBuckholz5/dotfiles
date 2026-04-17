303702d01ebf94ecf34a9f2e0aa57445d409f606
Trent Buckholz <trent.buckholz@chalk.ai>
2026-04-16 18:59:27 -0700

    fix(nvim): Attempt to fix conflicting vcs signs

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,11 @@
+local function is_jj_repo()
+  return vim.fn.finddir('.jj', vim.fn.getcwd() .. ';') ~= ''
+end
+
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
   lazy = false,
+  cond = function() return not is_jj_repo() end,
   opts = {
     signs = {
       add = { text = '+' },
--- nvim/lua/plugins/jjsigns.lua
+++ nvim/lua/plugins/jjsigns.lua
@@ -1,6 +1,18 @@
+local function is_jj_repo()
+  return vim.fn.finddir('.jj', vim.fn.getcwd() .. ';') ~= ''
+end
+
 return {
   'evanphx/jjsigns.nvim',
+  cond = is_jj_repo,
   config = function()
     require('jjsigns').setup()
+
+    vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufEnter', 'FocusGained' }, {
+      callback = function()
+        local ok, jjsigns = pcall(require, 'jjsigns')
+        if ok and jjsigns.update then jjsigns.update() end
+      end,
+    })
   end,
 }
